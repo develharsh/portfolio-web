73d3020a4296c3b2454980fa7b6cecec87815718
Harshvardhan Singh <51012630+develharsh@users.noreply.github.com>
2023-01-25 02:48:05 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (_, res) => {
-    res.status(404).render("index", {});
+    res.status(200).render("index", {});
 })
 
 module.exports = router;
